9cbc372cd225d1494d9c68cbb822872901f571db
rithick2705 <tvu36424u18038@gmail.com>
2025-08-30 21:46:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you for reaching out, Rithick will get back to you soon!");
+});
